98e1e05b27fb22059379324baf263a863139cdf6
Nick Hodapp <nick@remitly.com>
2022-11-18 13:45:53 -0800

    changes to support iOS 12

--- RemitlyCEKit/UIApplication+Remitly.swift
+++ RemitlyCEKit/UIApplication+Remitly.swift
@@ -9,17 +9,21 @@ import Foundation
 
 extension UIApplication {
     
-    private var keyWindow: UIWindow? {
-        return UIApplication
-            .shared
-            .connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
-            .first { $0.isKeyWindow }
+    private var rmKeyWindow: UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication
+                .shared
+                .connectedScenes
+                .filter { $0.activationState == .foregroundActive }
+                .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+                .first { $0.isKeyWindow }
+        } else {
+            return UIApplication.shared.keyWindow;
+        }
     }
     
     internal var keyWindowPresentedController: UIViewController? {
-        var viewController = self.keyWindow?.rootViewController
+        var viewController = self.rmKeyWindow?.rootViewController
       
         while let presentedController = viewController?.presentedViewController {
             if let presentedController = presentedController as? UITabBarController {
